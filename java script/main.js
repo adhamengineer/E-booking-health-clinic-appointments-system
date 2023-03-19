98842966bb3a3eb73737d02299bf6fa63ce8eaeb
moath00 <moath.n.h20@gmail.com>
2023-03-19 13:33:36 +0200

    edit ancor links

--- java script/main.js
+++ java script/main.js
@@ -1,3 +1,17 @@
 function filtering(selection) {
-    
+    var filterItems = document.getElementsByClassName('filter-item');
+    items = []
+    for (var i = 0; i < filterItems.length; i++) {
+        items[i] = filterItems[i];
+    }
+    console.log(items);
+    for (var i = 0; i < filterItems.length; i++) {
+        if (filterItems[i].className.includes(selection)){
+            filterItems[i].setAttribute('class', filterItems[i].className + ' d-none');
+        }
+        else
+        {
+            filterItems[i].setAttribute('class', filterItems[i].className + ' d-inline-block');
+        }
+    }
 }
